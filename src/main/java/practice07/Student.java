938099b236f7238bf625e22489cb958f04779941
BaiYunfei-TW <baiyunfei_tw@foxmail.com>
2018-04-24 20:23:04 +0800

    pracitce 08 passed

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,28 @@
 package practice07;
 
-public class Student {
+
+public class Student extends Person {
+
+    private Klass klass;
+
+    public Student(String name, int age, Klass kClass) {
+        super(name, age);
+        this.klass = kClass;
+    }
+
+    public String introduce(){
+        return super.introduce() + " I am a Student. I am at Class " + klass.getNumber() + ".";
+    }
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
 }

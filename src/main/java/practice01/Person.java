35f43ed957013ab4d0758edea988ee3708ff0c19
BaiYunfei-TW <baiyunfei_tw@foxmail.com>
2018-04-24 20:05:27 +0800

    practice01 passed

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,41 @@
 package practice01;
 
 public class Person {
+
+    private String name;
+    private int age;
+    private String introduction = "My name is Tom. I am 21 years old.";
+
+    public Person(String name, int age) {
+        this.name = name ;
+        this.age = age;
+    }
+
+    public String introduce(){
+        return getIntroduction();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
 }

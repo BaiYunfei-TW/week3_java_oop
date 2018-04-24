94de47a8a8c74e5773c6efcbf47324a4cce4c2d0
BaiYunfei-TW <baiyunfei_tw@foxmail.com>
2018-04-24 20:56:32 +0800

    practice 11 passed

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,82 @@
 package practice11;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Klass {
+
+    private Integer number;
+
+    private Person leader;
+
+    private Set<Student> students = new HashSet<>();
+
+    private Set<Teacher> teachers = new HashSet<>();
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public String getDisplayName(){
+        return "Class " + number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Person getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Person leader) {
+        if (!students.contains(leader)) {
+            System.out.print("It is not one of us.\n");
+            return;
+        }
+        this.leader = leader;
+
+        for (Teacher teacher : teachers) {
+            System.out.printf("I am %s. I know %s become Leader of Class %d.\n", teacher.getName(), leader.getName(), number);
+        }
+    }
+
+    public void appendMember(Student student) {
+        students.add(student);
+        for (Teacher teacher : teachers) {
+            System.out.printf("I am %s. I know %s has joined Class %d.\n", teacher.getName(), student.getName(), number);
+        }
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Klass && this.getNumber() == ((Klass) obj).getNumber()) {
+            return true;
+        }
+        return false;
+    }
+
+    public Set<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(Set<Teacher> teachers) {
+        this.teachers = teachers;
+    }
+
+    public void addTeacher(Teacher teacher) {
+        this.teachers.add(teacher);
+    }
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,73 @@
 package practice11;
 
-public class Teacher {
+import java.util.HashSet;
+import java.util.Set;
+
+public class Teacher extends Person {
+
+    private Set<Klass> classes = new HashSet<>();
+
+    public Teacher(String name, int age, Set<Klass> klassSet) {
+        super(name, age);
+        this.classes = klassSet;
+        for (Klass klass : klassSet) {
+            klass.addTeacher(this);
+        }
+    }
+
+    public Teacher(Integer id, String name, Integer age) {
+        super(id, name, age);
+    }
+
+    public Teacher(Integer id, String name, int age, HashSet<Klass> klassSet) {
+        super(id, name, age);
+        this.classes = klassSet;
+        for (Klass klass : klassSet) {
+            klass.addTeacher(this);
+        }
+    }
+
+    public String introduce(){
+        if(classes.size() == 0){
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+        return super.introduce() + " I am a Teacher. I teach " + this.getClassNameString() + ".";
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    private String getClassNameString(){
+        String classNameString = "Class ";
+        for (Klass klass : classes) {
+            classNameString += klass.getNumber() + ", ";
+        }
+        if(classNameString.length() > 6){
+            classNameString = classNameString.substring(0, classNameString.length() - 2);
+        }
+        return classNameString;
+    }
+
+    public Set<Klass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(Set<Klass> classes) {
+        this.classes = classes;
+    }
+
+    public boolean isTeaching(Student student) {
+        if (this.classes.contains(student.getKlass())) {
+            return true;
+        }
+        return false;
+    }
+
+    public String introduceWith(Student student) {
+        if (student != null && this.classes.contains(student.getKlass())) {
+            return super.introduce() + " I am a Teacher. I teach Jerry.";
+        }
+        return super.introduce() + " I am a Teacher. I don't teach Jerry.";
+    }
 }

2cfe644502829108ae80fe75b6707aa094321b04
BaiYunfei-TW <baiyunfei_tw@foxmail.com>
2018-04-24 20:10:01 +0800

    practice 04 passed

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,28 @@
 package practice04;
 
-public class Student {
+
+public class Student extends Person {
+
+    private int klass;
+
+    public Student(String name, int age, int kClass) {
+        super(name, age);
+        this.klass = kClass;
+    }
+
+    public String introduce(){
+        return super.introduce() + " I am a Student. I am at Class 2.";
+    }
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
 }

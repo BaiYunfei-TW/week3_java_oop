af9fe65dc010f248da4f74783cd37cd7cb556869
BaiYunfei-TW <baiyunfei_tw@foxmail.com>
2018-04-24 20:15:04 +0800

    practice 06 passed

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,32 @@
 package practice06;
 
-public class Teacher {
+
+public class Teacher extends Person {
+
+    private Integer klass;
+
+    public Teacher(String name, int age, int kClass) {
+        super(name, age);
+        this.klass = kClass;
+    }
+
+    public String introduce(){
+        String teacherIntroduction = "I am a Teacher. I teach Class "+klass+".";
+        if(klass == null){
+            teacherIntroduction = "I am a Teacher. I teach No Class.";
+        }
+        return super.introduce() + " " + teacherIntroduction;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
 }
